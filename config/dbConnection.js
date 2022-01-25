098915fc54b421f37da9068cbdf0204c0b57d341
andrealargo19 <andrealargo@JENNIFERs-MacBook-Pro.local>
2022-01-25 23:26:04 +0100

    configurando base de datos

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,9 +2,10 @@ const mysql = require("promise-mysql");
 
 module.exports = async () => {
   return await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWD,
-    database: process.env.DB_NAME,
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b98a363b07af2c',
+    password: '1d2c5681',
+    database: 'heroku_d2458b5b17a2bd6',
   });
 };
+
